e1f9aa7dd0ba33f7e7d117c806e900433cbb96b0
Enes <66560757+devenes@users.noreply.github.com>
2022-05-11 14:48:13 +0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -6,6 +6,5 @@ mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
-
-export PS1="\[\e[1;34m\]\u\[\e[33m\]@\h# \W:\[\e[32m\]\\$\[\e[m\]"
-alias k="kubectl"
+echo 'export PS1="\[\e[1;34m\]\u\[\e[33m\]@\h# \W:\[\e[32m\]\\$\[\e[m\]"' >> ~/.bashrc
+echo "alias k='kubectl'" >> ~/.bashrc
