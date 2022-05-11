00eed7a3f000a67fe5c8282e2c022a8e0d5bc7c0
Enes <66560757+devenes@users.noreply.github.com>
2022-05-11 14:29:35 +0300

    Create userdata.sh

---
+++ userdata.sh
@@ -0,0 +1,11 @@
+sudo yum update -y
+sudo yum install git -y
+curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.22.6/2022-03-09/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
+export PS1="\[\e[1;34m\]\u\[\e[33m\]@\h# \W:\[\e[32m\]\\$\[\e[m\]"
+alias k="kubectl"
